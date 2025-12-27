b47e4f92f6a2d6933db9cd53d9f57545cccbdbd0
Draghici Denis Cosmin <154325453+DenisCosmin21@users.noreply.github.com>
2025-12-27 14:50:59 +0200

    Added define for work and stop work to utilities to use them as tags on receive on workers, to differentiate operation

--- Utilities/Utilities.h
+++ Utilities/Utilities.h
@@ -4,7 +4,10 @@
 
 #ifndef UTILITIES_H
 #define UTILITIES_H
+#define WORK 0
+#define STOP_WORKING 1
 #include <stddef.h>
+
 
 void *allocate(void *buffer, size_t elem_size, size_t *old_size);
 #endif //UTILITIES_H
