1d17910ee514dac7f19ccec2f4dc2eae1d787969
Draghici Denis Cosmin <154325453+DenisCosmin21@users.noreply.github.com>
2026-01-13 19:08:17 +0200

    Fixed the allocation scheme that used to shorten the memory

--- Utilities/Utilities.c
+++ Utilities/Utilities.c
@@ -8,7 +8,7 @@
 #include <stdlib.h>
 
 void *allocate(void *buffer, size_t elem_size, size_t *old_size) {
-    *old_size = *old_size < 4 ? *old_size + 1 : (*old_size * 2) / 3;
+    *old_size = *old_size < 4 ? *old_size + 1 : (*old_size / 2) * 3;
 
     void *new_buffer = realloc(buffer, *old_size * elem_size);
 
