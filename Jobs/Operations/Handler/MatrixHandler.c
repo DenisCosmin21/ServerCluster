dc5b8d313c5b4c9b7e71012513c521b6351e8fe3
Draghici Denis Cosmin <154325453+DenisCosmin21@users.noreply.github.com>
2026-01-14 21:28:09 +0200

    Fixed a problem with secondMatrix that generated segmentation fault because it was the same address on each job, and when a job got deleted, the string got too

--- Jobs/Operations/Handler/MatrixHandler.c
+++ Jobs/Operations/Handler/MatrixHandler.c
@@ -18,6 +18,9 @@ static void readMatrixChunk(FILE *file, char *buffer, size_t chunkSize, size_t m
         fgets(buffer + charsRead, matrixSize * 9, file);
         charsRead = strlen(buffer);
     }
+
+    if(buffer[charsRead - 1] == '\n')
+        buffer[charsRead - 1] = '\0';
 }
 
 static size_t getMatrixSize(FILE *file) { //First 2 elements of the matrix represent the size
@@ -28,7 +31,7 @@ static size_t getMatrixSize(FILE *file) { //First 2 elements of the matrix repre
     return size;
 }
 
-void MatrixMultHandler(job_t job) {
+void matrixMultHandler(job_t job) {
     char *params = job->params;
     char *fileName1 = strtok(params, " ");
     char *fileName2 = strtok(NULL, " ");
@@ -42,9 +45,11 @@ void MatrixMultHandler(job_t job) {
 
     size_t matrixSize = getMatrixSize(file2);
 
-    char *secondMatrix = malloc(matrixSize * sizeof(char) * 9 * matrixSize);
+    char *secondMatrix = malloc(matrixSize * sizeof(char) * 9 * matrixSize + 8);
 
-    readMatrixChunk(file2, secondMatrix, matrixSize, matrixSize);
+    sprintf(secondMatrix, "%llu\n", matrixSize);
+
+    readMatrixChunk(file2, secondMatrix + strlen(secondMatrix), matrixSize, matrixSize);
 
     fclose(file2);
 
@@ -74,27 +79,49 @@ void MatrixMultHandler(job_t job) {
         size_t chunkSize = matrixSize / MAX_JOBS_PER_JOB;
         size_t lastChunk = matrixSize % MAX_JOBS_PER_JOB + chunkSize;
 
-        while(chunk <= MAX_JOBS_PER_JOB) {
-            firstMatrix = malloc(chunkSize * sizeof(char) * 9 * matrixSize);
+        job_t jobToAdd = NULL;
 
-            if(firstMatrix == NULL) {
-                perror("malloc");
-                exit(-1);
+        while(chunk <= MAX_JOBS_PER_JOB) {
+            if(chunk == MAX_JOBS_PER_JOB) {
+                firstMatrix = malloc(lastChunk * sizeof(char) * 9 * matrixSize);
+
+                if(firstMatrix == NULL) {
+                    perror("malloc");
+                    exit(-1);
+                }
+
+                readMatrixChunk(file1, firstMatrix, lastChunk, matrixSize); //If we are on the last job created we should span it on all the lines
+
+                jobToAdd = newJob(MATRIXMULT, firstMatrix, secondMatrix, job->jobId, chunk);
             }
 
-            if(chunk == MAX_JOBS_PER_JOB)
-                readMatrixChunk(file1, firstMatrix, lastChunk, matrixSize); //If we are on the last job created we should span it on all the lines
-            else
+            else {
+                char *secondMatrixCopy = malloc(matrixSize * sizeof(char) * 9 * matrixSize + 8);
+
+                memset(secondMatrixCopy, 0, 1);
+                strcpy(secondMatrixCopy, secondMatrix);
+
+                firstMatrix = malloc(chunkSize * sizeof(char) * 9 * matrixSize);
+
+                if(firstMatrix == NULL) {
+                    perror("malloc");
+                    exit(-1);
+                }
+
                 readMatrixChunk(file1, firstMatrix, chunkSize, matrixSize);
 
-            job_t jobToAdd = newJob(MATRIXMULT, firstMatrix, secondMatrix, job->jobId, chunk);
+                jobToAdd = newJob(MATRIXMULT, firstMatrix, secondMatrixCopy, job->jobId, chunk);
+            }
 
             enqueueJob(jobToAdd);
+
             chunk++;
         }
     }
 
-     destructJob(job);
+
+    fclose(file1);
+    destructJob(job);
 }
 
 void matrixAddHandler(job_t job) {
